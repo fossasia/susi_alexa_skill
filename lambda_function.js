bd15f8b25c06724f3099bf250e071663ff2944bd
Manveer695 <singhmanveer1995@gmail.com>
2017-07-15 11:34:36 +0530

    corrected table type response

--- lambda_function.js
+++ lambda_function.js
@@ -29,23 +29,41 @@ exports.handler = (event, context) => {
         switch(event.request.intent.name) {
           case "callSusiApi":
             console.log(event.request.intent.slots.query.value)
+            var viewCount;
             var endpoint = "http://api.susi.ai/susi/chat.json?q="+event.request.intent.slots.query.value; // ENDPOINT GOES HERE
             var body = ""
             https.get(endpoint, (response) => {
               response.on('data', (chunk) => { body += chunk })
               response.on('end', () => {
-                var data = JSON.parse(body)
-                var viewCount = data.answers[0].actions[0].expression;
+                var data = JSON.parse(body);
+                if(data.answers[0].actions[1]){
+                  if(data.answers[0].actions[1].type === 'rss'){
+                    viewCount = 'I have no idea about it, sorry.';
+                  }
+                }
+                else{
+                  if(data.answers[0].actions[0].type === 'table'){
+                    var colNames = data.answers[0].actions[0].columns;
+                    viewCount = '';
+                    if((data.answers[0].metadata.count)>10)
+                      viewCount += 'Due to message limit, only some results are spoke. They are: ';
+                    for(var i=0;i<(((data.answers[0].metadata.count)>10)?10:data.answers[0].metadata.count);i++){
+                      viewCount += data.answers[0].data[i].name+',';
+                    }
+                  }
+                  else
+                  {
+                    viewCount = data.answers[0].actions[0].expression;
+                  }
+                }
                 context.succeed(
                   generateResponse(
-                    buildSpeechletResponse(`${viewCount}`, true),
-                    {}
+                    buildSpeechletResponse(`${viewCount}`, true),{}
                   )
                 )
               })
             })
             break;
-
           default:
             throw "Invalid intent"
         }
